fcb0aafa094b1dd1e90af68d4a0ae58200ffd571
Keith Stanley <iamstan13y@gmail.com>
2023-10-18 17:50:40 +0200

    Added CallLog Model.

---
+++ Pegasus.API/Models/Data/CallLog.cs
@@ -0,0 +1,13 @@
+﻿namespace Pegasus.API.Models.Data;
+
+public class CallLog
+{
+    public int Id { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? ContactName { get; set; }
+    public string? Direction { get; set; }
+    public DateTime CallDate { get; set; }
+    public int Duration { get; set; }
+    public string? Notes { get; set; }
+    public DateTime LogDate { get; set; } = DateTime.Now;
+}
