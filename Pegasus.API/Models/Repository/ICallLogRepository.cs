8f825d2709e41504ae37632aefca750119ae8c0b
Keith Stanley <iamstan13y@gmail.com>
2023-10-19 09:09:19 +0200

    CallLog Repository Pattern Config.

---
+++ Pegasus.API/Models/Repository/ICallLogRepository.cs
@@ -0,0 +1,11 @@
+﻿using Pegasus.API.Models.Data;
+using Pegasus.API.Models.Local;
+
+namespace Pegasus.API.Models.Repository;
+
+public interface ICallLogRepository
+{
+    Task<Result<CallLog>> AddAsync(CallLog callLog);
+    Task<Result<IEnumerable<CallLog>>> GetAllAsync();
+    Task<Result<CallLog>> GetByIdAsync(int id);
+}
