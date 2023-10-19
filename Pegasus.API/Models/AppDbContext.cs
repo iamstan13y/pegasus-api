85836ffdac58d90c62d8769d050dbc556507bbe0
Keith Stanley <iamstan13y@gmail.com>
2023-10-19 08:41:38 +0200

    CallLog Model & DB Migrations.

---
+++ Pegasus.API/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pegasus.API.Models.Data;
+
+namespace Pegasus.API.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<CallLog>? CallLogs { get; set; }
+}
