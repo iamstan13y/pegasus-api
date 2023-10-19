683eb2b7955887fe539cd657cb2a7d0a1bd82c4a
Keith Stanley <iamstan13y@gmail.com>
2023-10-19 10:27:08 +0200

    Record Call Log Implementation.

---
+++ Pegasus.API/DateTimeExtenstions.cs
@@ -0,0 +1,12 @@
+﻿namespace Pegasus.API;
+
+public static class DateTimeExtensions
+{
+    private static readonly DateTimeOffset UnixEpoch = new(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+    public static DateTime ToDateTime(this long timestamp)
+    {
+        DateTimeOffset dateTimeOffset = UnixEpoch.AddMilliseconds(timestamp);
+        return dateTimeOffset.DateTime.AddHours(2);
+    }
+}
