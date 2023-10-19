e45fc8f50111a32ff92c173b6acebd16fbd5db27
Keith Stanley <iamstan13y@gmail.com>
2023-10-19 09:16:55 +0200

    Added CallLogController.

---
+++ Pegasus.API/Controllers/CallLogController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pegasus.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CallLogController : ControllerBase
+    {
+    }
+}
