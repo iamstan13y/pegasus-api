e7ac8ddcb630d2fafc64831a2d64947306a4b26e
Keith Stanley <iamstan13y@gmail.com>
2023-10-19 10:26:59 +0200

    Record Call Log Implementation.

--- Pegasus.API/Controllers/CallLogController.cs
+++ Pegasus.API/Controllers/CallLogController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Pegasus.API.Models.Data;
+using Pegasus.API.Models.Local;
+using Pegasus.API.Models.Repository;
 
 namespace Pegasus.API.Controllers
 {
@@ -6,5 +9,26 @@ namespace Pegasus.API.Controllers
     [ApiController]
     public class CallLogController : ControllerBase
     {
+        private readonly ICallLogRepository _repository;
+
+        public CallLogController(ICallLogRepository repository) => _repository = repository;
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] CallLogRequest request)
+        {
+            var result = await _repository.AddAsync(new CallLog
+            {
+                ContactName = request.Contact,
+                CallDate = request.Date.ToDateTime(),
+                Direction = request.Direction,
+                Duration = request.Duration,
+                Notes = request.Notes,
+                PhoneNumber = request.Number
+            });
+
+            if (!result.Success) return BadRequest(result);
+
+            return Ok(result);
+        }
     }
 }
