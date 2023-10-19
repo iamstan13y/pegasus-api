d9d41c708df735482764464db134669b49735d35
Keith Stanley <iamstan13y@gmail.com>
2023-10-19 10:27:02 +0200

    Record Call Log Implementation.

---
+++ Pegasus.API/Models/Local/CallLogRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Pegasus.API.Models.Local;
+
+public class CallLogRequest
+{
+    public string? Number { get; set; }
+    public string? Contact { get; set; }
+    public string? Direction { get; set; }
+    public long Date { get; set; }
+    public int Duration { get; set; }
+    public string? Notes { get; set; }
+}
