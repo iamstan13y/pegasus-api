9973ac2d4aa34ee94e9b1995d324375766c5227b
Keith Stanley <iamstan13y@gmail.com>
2023-10-19 09:09:21 +0200

    CallLog Repository Pattern Config.

--- Pegasus.API/Program.cs
+++ Pegasus.API/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using Pegasus.API.Models;
+using Pegasus.API.Models.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICallLogRepository, CallLogRepository>();
 
 builder.Services.AddControllers();
 
