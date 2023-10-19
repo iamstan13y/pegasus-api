61617b65eaf64cbb81f05a8c3656e8d80b31f0d9
Keith Stanley <iamstan13y@gmail.com>
2023-10-19 08:41:40 +0200

    CallLog Model & DB Migrations.

--- Pegasus.API/Program.cs
+++ Pegasus.API/Program.cs
@@ -1,15 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Pegasus.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
