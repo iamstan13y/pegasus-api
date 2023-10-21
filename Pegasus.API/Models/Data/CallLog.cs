dae098b1eae6e218de585ced6c3d559678808d4b
Keith Stanley <iamstan13y@gmail.com>
2023-10-21 14:08:01 +0200

    Call Type & DB Migrations + Input Formatting.

--- Pegasus.API/Models/Data/CallLog.cs
+++ Pegasus.API/Models/Data/CallLog.cs
@@ -1,11 +1,13 @@
-﻿namespace Pegasus.API.Models.Data;
+﻿using Pegasus.API.Models.Enums;
+
+namespace Pegasus.API.Models.Data;
 
 public class CallLog
 {
     public int Id { get; set; }
     public string? PhoneNumber { get; set; }
     public string? ContactName { get; set; }
-    public string? Direction { get; set; }
+    public string? Type { get; set; }
     public DateTime CallDate { get; set; }
     public int Duration { get; set; }
     public string? Notes { get; set; }
