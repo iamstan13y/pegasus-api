05865b94a5dc1c9f27d6147e56137702ea2efa66
Keith Stanley <iamstan13y@gmail.com>
2023-10-21 09:47:44 +0200

    Input Formatting Extensions.

---
+++ Pegasus.API/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Pegasus.API.Extensions;
+
+public static class StringExtensions
+{
+    public static string SanitizePhoneNumber(this string encodedPhoneNumber)
+    {
+        string decodedPhoneNumber = Uri.UnescapeDataString(encodedPhoneNumber);
+        decodedPhoneNumber = decodedPhoneNumber.Replace("+", string.Empty);
+        return decodedPhoneNumber;
+    }
+
+    public static string Sanitize(this string encodedText) => encodedText.Replace("+", " ");
+    
+}
