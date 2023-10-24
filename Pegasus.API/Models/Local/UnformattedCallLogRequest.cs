b091342a6a07b9550c51a6c0d481a10e00951de2
Keith Stanley <iamstan13y@gmail.com>
2023-10-24 08:31:39 +0200

    Importing Call Logs functionality.

---
+++ Pegasus.API/Models/Local/UnformattedCallLogRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Pegasus.API.Models.Local;
+
+public class UnformattedCallLogRequest
+{
+    public string? Number { get; set; }
+    public string? Type { get; set; }
+    public string? Date { get; set; }
+    public string? Time { get; set; }
+    public int Duration { get; set; }
+    public string? Contact { get; set; }
+}
