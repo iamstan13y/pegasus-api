e861d69c5c53f53e0552e0d06aa8d08d89e7b722
Keith Stanley <iamstan13y@gmail.com>
2023-10-24 08:31:33 +0200

    Importing Call Logs functionality.

---
+++ Pegasus.API/Extensions/DataExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Pegasus.API.Models.Data;
+using Pegasus.API.Models.Local;
+using System.Globalization;
+
+namespace Pegasus.API.Extensions;
+
+public static class DataExtensions
+{
+    public static List<CallLog> SanitizeData(this List<UnformattedCallLogRequest> data)
+    {
+        var sanizitedData = new List<CallLog>();
+
+        foreach (var callLog in data)
+        {
+            sanizitedData.Add(new CallLog
+            {
+                ContactName = callLog.Contact?.Sanitize(),
+                Type = callLog.Type,
+                CallDate = DateTime.ParseExact($"{callLog.Date} {callLog.Time}", "dd-MMM-yy HH:mm", CultureInfo.InvariantCulture),
+                Duration = callLog.Duration,
+                PhoneNumber = callLog.Number?.SanitizePhoneNumber()
+            });
+        }
+
+        return sanizitedData;
+    }
+}
