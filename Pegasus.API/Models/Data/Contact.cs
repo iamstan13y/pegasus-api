659dfb214840c6a206c99e91c80683d921dedcbc
Keith Stanley <iamstan13y@gmail.com>
2024-07-19 23:45:50 +0200

    Redesigned the Models.

---
+++ Pegasus.API/Models/Data/Contact.cs
@@ -0,0 +1,8 @@
+﻿namespace Pegasus.API.Models.Data;
+
+public class Contact
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<ContactPhoneNumber> PhoneNumbers { get; set; } = new List<ContactPhoneNumber>();
+}
