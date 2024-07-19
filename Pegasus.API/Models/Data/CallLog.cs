b498dbd629de34a3663d41d4f012280aa09f22d3
Keith Stanley <iamstan13y@gmail.com>
2024-07-19 23:45:46 +0200

    Redesigned the Models.

--- Pegasus.API/Models/Data/CallLog.cs
+++ Pegasus.API/Models/Data/CallLog.cs
@@ -5,11 +5,11 @@ namespace Pegasus.API.Models.Data;
 public class CallLog
 {
     public int Id { get; set; }
-    public string? PhoneNumber { get; set; }
-    public string? ContactName { get; set; }
-    public string? Type { get; set; }
+    public int? ContactId { get; set; }
+    public CallType Type { get; set; }
     public DateTime CallDate { get; set; }
     public int Duration { get; set; }
     public string? Notes { get; set; }
     public DateTime LogDate { get; set; } = DateTime.Now;
+    public Contact? Contact { get; set; }
 }
