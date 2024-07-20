f066ef43e319daf34d50e0978db60e2817c9c15f
Keith Stanley <iamstan13y@gmail.com>
2024-07-21 00:43:01 +0200

    Add Call Log Implementation v2.0

---
+++ Pegasus.API/Extensions/JsonExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Pegasus.API.Extensions;
+
+public static class JsonExtensions
+{
+    public static T? RemoveCycles<T>(this T obj)
+    {
+        var options = new JsonSerializerOptions
+        {
+            ReferenceHandler = ReferenceHandler.IgnoreCycles,
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        var serializedObj = JsonSerializer.Serialize(obj, options);
+
+        return JsonSerializer.Deserialize<T?>(serializedObj, options);
+    }
+}
