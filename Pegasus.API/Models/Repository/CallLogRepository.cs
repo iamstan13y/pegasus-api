625ca299aa4cb6e33f13ce82771cb402b3313f41
Keith Stanley <iamstan13y@gmail.com>
2024-07-21 00:43:05 +0200

    Add Call Log Implementation v2.0

--- Pegasus.API/Models/Repository/CallLogRepository.cs
+++ Pegasus.API/Models/Repository/CallLogRepository.cs
@@ -1,4 +1,7 @@
-﻿using Pegasus.API.Models.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Pegasus.API.Extensions;
+using Pegasus.API.Models.Data;
+using Pegasus.API.Models.Enums;
 using Pegasus.API.Models.Local;
 
 namespace Pegasus.API.Models.Repository;
@@ -8,21 +11,78 @@ public class CallLogRepository : ICallLogRepository
     private readonly AppDbContext _context;
 
     public CallLogRepository(AppDbContext context) => _context = context;
-    
-    public async Task<Result<CallLog>> AddAsync(CallLog callLog)
+
+    public async Task<Result<CallLog>> AddAsync(CallLogRequest callLogRequest)
     {
         try
         {
+            var phoneNumber = callLogRequest.Number?.SanitizePhoneNumber();
+            var contactName = callLogRequest.Contact?.Sanitize();
+
+            var contactPhoneNumber = await _context.ContactPhoneNumbers!
+                .Include(cpn => cpn.Contact)
+                .FirstOrDefaultAsync(cpn => cpn.PhoneNumber == phoneNumber);
+
+            Contact? contact = null;
+
+            if (contactPhoneNumber != null)
+            {
+                contact = contactPhoneNumber.Contact;
+            }
+            else
+            {
+                contact = await _context.Contacts!
+                    .Include(c => c.PhoneNumbers)
+                    .FirstOrDefaultAsync(c => c.Name == contactName);
+
+                if (contact != null)
+                {
+                    if (!contact.PhoneNumbers.Any(pn => pn.PhoneNumber == phoneNumber))
+                    {
+                        contact.PhoneNumbers.Add(new ContactPhoneNumber
+                        {
+                            PhoneNumber = phoneNumber
+                        });
+                    }
+                }
+                else
+                {
+                    contact = new Contact
+                    {
+                        Name = contactName,
+                        PhoneNumbers = new List<ContactPhoneNumber>
+                    {
+                        new()
+                        {
+                            PhoneNumber = phoneNumber
+                        }
+                    }
+                    };
+
+                    await _context.Contacts!.AddAsync(contact);
+                }
+            }
+
+            var callLog = new CallLog
+            {
+                Contact = contact,
+                Type = (CallType)callLogRequest.Direction,
+                CallDate = callLogRequest.Date.ToDateTime(),
+                Duration = callLogRequest.Duration,
+                Notes = callLogRequest.Notes?.Sanitize()
+            };
+
             await _context.CallLogs!.AddAsync(callLog);
             await _context.SaveChangesAsync();
 
-            return new Result<CallLog>(callLog);
+            return new Result<CallLog>(callLog.RemoveCycles()!);
         }
         catch (Exception ex)
         {
-            return new Result<CallLog>(false, $"An error occured:{ex.Message}");
+            return new Result<CallLog>(false, $"An error occurred: {ex.Message}");
         }
     }
+
 
     public async Task<Result<string>> AddBulkAsync(List<CallLog> callLogs)
     {
