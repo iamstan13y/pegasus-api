d1a89d2917fee5ac2a251f7612d1d604899d5227
Keith Stanley <iamstan13y@gmail.com>
2024-07-23 16:59:15 +0200

    Minor Updates in Contact.

--- Pegasus.API/Models/Data/Contact.cs
+++ Pegasus.API/Models/Data/Contact.cs
@@ -2,7 +2,7 @@
 
 public class Contact
 {
-    public int Id { get; set; }
+    public long Id { get; set; }
     public string Name { get; set; }
     public List<ContactPhoneNumber> PhoneNumbers { get; set; } = new List<ContactPhoneNumber>();
 }
