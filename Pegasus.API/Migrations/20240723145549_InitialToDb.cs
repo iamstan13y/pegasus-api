7e85dab438f6c8b0236ae1151998da793f8c156c
Keith Stanley <iamstan13y@gmail.com>
2024-07-23 16:58:10 +0200

    Migrations Fix.

---
+++ Pegasus.API/Migrations/20240723145549_InitialToDb.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pegasus.API.Migrations
+{
+    public partial class InitialToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CallLogs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContactId = table.Column<long>(type: "bigint", nullable: true),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    CallDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LogDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CallLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CallLogs_Contacts_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "Contacts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContactPhoneNumbers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactPhoneNumbers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ContactPhoneNumbers_Contacts_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "Contacts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CallLogs_ContactId",
+                table: "CallLogs",
+                column: "ContactId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactPhoneNumbers_ContactId",
+                table: "ContactPhoneNumbers",
+                column: "ContactId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CallLogs");
+
+            migrationBuilder.DropTable(
+                name: "ContactPhoneNumbers");
+
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
