3c207a87990ab69c43a7df1d6f6fc66def8d7bde
Keith Stanley <iamstan13y@gmail.com>
2024-07-24 17:22:19 +0200

    More Fixes & Stuff.

---
+++ Pegasus.API/Migrations/20240724151607_CascadeToDb.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pegasus.API.Migrations
+{
+    public partial class CascadeToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CallLogs_Contacts_ContactId",
+                table: "CallLogs");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CallLogs_Contacts_ContactId",
+                table: "CallLogs",
+                column: "ContactId",
+                principalTable: "Contacts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CallLogs_Contacts_ContactId",
+                table: "CallLogs");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CallLogs_Contacts_ContactId",
+                table: "CallLogs",
+                column: "ContactId",
+                principalTable: "Contacts",
+                principalColumn: "Id");
+        }
+    }
+}
